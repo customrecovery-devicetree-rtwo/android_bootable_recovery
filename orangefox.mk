8f8729a926b9c15a78cb5cad6a35678cd49db3ba
Darth9 <reyprimerjefe@gmail.com>
2025-04-29 11:07:47 +0100

    Add warning

--- orangefox.mk
+++ orangefox.mk
@@ -159,6 +159,7 @@ endif
 ifeq ($(FOX_AB_DEVICE),1)
     LOCAL_CFLAGS += -DFOX_AB_DEVICE='"1"'
     ifneq ($(AB_OTA_UPDATER),true)
+    	$(warning Please add 'AB_OTA_UPDATER:= true' to your BoardConfig.mk)
     	LOCAL_CFLAGS += -DAB_OTA_UPDATER=1
     	LOCAL_SHARED_LIBRARIES += libhardware android.hardware.boot@1.0
     	TWRP_REQUIRED_MODULES += libhardware android.hardware.boot@1.0-service android.hardware.boot@1.0-service.rc
